b2b422aa1475cf9c140a07f408ed0581cc782d70
Guillaume Seznec <guillaume@seznec.fr>
2020-02-09 19:32:05 +0100

    color

--- app/lib/show.js
+++ app/lib/show.js
@@ -27,7 +27,7 @@ function readGridFile(gridFile) {
       let [startTime, endTime] = showTimes.split('-');
       obj[dayName][startTime] = {
         title: showTitle,
-        color: '#bfa267',
+        color: '#dfa14d',
         color_alt: '#ffffff',
         start: absDayMTS + (startTime.substr(0, 2) * 3600 + startTime.substr(2, 2) * 60) * 1000,
         end: absDayMTS + (endTime.substr(0, 2) * 3600 + endTime.substr(2, 2) * 60) * 1000,
